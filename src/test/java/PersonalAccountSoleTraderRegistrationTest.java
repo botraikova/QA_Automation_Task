27ab99f882b84429269b13c7f8962128873133ca
slayfen <147399701+slayfen@users.noreply.github.com>
2024-10-26 17:49:02 +0300

    Test

--- src/test/java/PersonalAccountSoleTraderRegistrationTest.java
+++ src/test/java/PersonalAccountSoleTraderRegistrationTest.java
@@ -1,2 +1,35 @@
-public class PersonalAccountSoleTraderRegistrationTest {
+import PageObject.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PersonalAccountSoleTraderRegistrationTest extends TestAndDriverSetup {
+    @Test
+
+    public void registrationForPersonalAccountSoleTraderTest(){
+
+        RegisterPage registerPage = new RegisterPage(webDriver);
+
+        registerPage.navigateTo();
+        Assert.assertTrue(registerPage.isPageLoaded(), "Register page isn't loaded");
+
+        registerPage.clickCookiesButton();
+
+        registerPage.clickPersonalAccount();
+        registerPage.clickSoleTraderForPersonal();
+
+        registerPage.typeAmount();
+        registerPage.typeFirstName();
+        registerPage.typeLastName();
+        registerPage.typeEmail();
+        registerPage.typeCellNumber();
+        registerPage.clickTermsAndCond();
+        registerPage.clickNewsAndOffers();
+        registerPage.enterMathResult();
+        registerPage.clickRegisterButton();
+        registerPage.navigateToVerificationPage();
+
+        Assert.assertTrue(registerPage.isVerifPageLoaded(), "Verification page isn't loaded");
+
+
+    }
 }
