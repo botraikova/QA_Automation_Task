c93325bc843a12cd36ac1a1794013059e74a185b
slayfen <147399701+slayfen@users.noreply.github.com>
2024-10-26 16:11:25 +0300

    Test 2

--- src/test/java/BusinessAccountPublicRegistrationTest.java
+++ src/test/java/BusinessAccountPublicRegistrationTest.java
@@ -1,2 +1,34 @@
-public class BusinessAccountPublicRegistrationTest {
-}
+import PageObject.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+    public class BusinessAccountPublicRegistrationTest extends TestAndDriverSetup {
+
+        @Test
+        public void registrationForBusinessAccountPrivateCompanyTest(){
+
+            RegisterPage registerPage = new RegisterPage(webDriver);
+
+            registerPage.navigateTo();
+            Assert.assertTrue(registerPage.isPageLoaded(), "Register page isn't loaded");
+            registerPage.clickCookiesButton();
+            registerPage.clickBusinessAccount();
+
+            registerPage.clickPublicCompanyForBusiness();
+
+            registerPage.typeLegalCompanyName();
+            registerPage.typeAmount();
+            registerPage.typeFirstName();
+            registerPage.typeLastName();
+            registerPage.typeEmail();
+            registerPage.typeCellNumber();
+            registerPage.clickTermsAndCond();
+            registerPage.clickNewsAndOffers();
+            registerPage.enterMathResult();
+            registerPage.clickRegisterButton();
+            registerPage.navigateToVerificationPage();
+
+            Assert.assertTrue(registerPage.isVerifPageLoaded(), "Verification page isn't loaded");
+
+        }
+    }
